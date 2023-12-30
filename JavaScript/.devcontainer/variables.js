f8627627af0dd6b650316ca676965309afc6e231
srmahitesh <141175991+srmahitesh@users.noreply.github.com>
2023-12-30 07:36:08 +0000

    variables files creaetd to understand their working

---
+++ JavaScript/.devcontainer/variables.js
@@ -0,0 +1,11 @@
+const Id=4656
+let Email="kashyaphitesh456@gmail.com"
+var PassWord="Hitesh@123"
+City="Hamirpur"
+
+console.log(Id);
+// Id=897
+// console.log(Id);
+console.log(Email);
+console.log(PassWord);
+console.log(City);
