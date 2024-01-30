7bc9a8f140c95e28a852f2ae22abd3f21a57cec9
srmahitesh <141175991+srmahitesh@users.noreply.github.com>
2024-01-30 17:28:53 +0000

    just progress saved

---
+++ JS/Strings08.js
@@ -0,0 +1,9 @@
+const name='Hitesh';
+const counter=10;
+console.log(name+" is on Rank"+counter); //old way of writing and not looks good
+//modern way
+console.log(`${name} is on Rank ${counter}`); //working & modern attractive
+
+let surname=new String("Sharma");
+console.log(surname.toUpperCase());
+console.log(surname);
